daaa2dab415f7bff4d9178109b6ca9dd1e71da9c
loot-factory <loot@videvago.com>
2021-09-10 19:37:58 +0200

    Formatting / Mint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React, { Component } from 'react';
-import logo  from './logo.png';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.png";
+import "./App.css";
 import { ethers } from "ethers";
 import Web3Modal from "web3modal";
 import WalletConnectProvider from "@walletconnect/web3-provider";
-import { LootBox } from './components/LootBox';
-
-const lootAbi = [{"inputs":[{"internalType":"address","name":"proxyRegistryAddress_","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"approved","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"operator","type":"address"},{"indexed":false,"internalType":"bool","name":"approved","type":"bool"}],"name":"ApprovalForAll","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"operator","type":"address"}],"name":"OperatorAdded","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":true,"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"operator","type":"address"}],"name":"addOperator","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"approve","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"customClaim","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"getApproved","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getKeys","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"uint256","name":"keyId","type":"uint256"}],"name":"getList","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"pure","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"operator","type":"address"}],"name":"isApprovedForAll","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"lootClaim","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"ownerOf","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"proxyRegistryAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"},{"internalType":"bytes","name":"_data","type":"bytes"}],"name":"safeTransferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"operator","type":"address"},{"internalType":"bool","name":"approved","type":"bool"}],"name":"setApprovalForAll","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"bytes4","name":"interfaceId","type":"bytes4"}],"name":"supportsInterface","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"uint256","name":"index","type":"uint256"}],"name":"tokenOfOwnerByIndex","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenURI","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"tokenValues","outputs":[{"internalType":"uint256[6][7]","name":"values","type":"uint256[6][7]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"tokenId","type":"uint256"}],"name":"transferFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"wallet","type":"address"}],"name":"walletClaim","outputs":[],"stateMutability":"nonpayable","type":"function"}];
+import { LootBox } from "./components/LootBox";
+import { lootAbi } from "./abi/lootmvp";
 
 interface IAppState {
   fetching: boolean;
@@ -29,86 +28,88 @@ const INITIAL_STATE: IAppState = {
   address: "",
   connected: false,
   chainId: 1,
-  networkName: 'mainnet',
+  networkName: "mainnet",
   showModal: false,
   pendingRequest: false,
   result: null,
   mintedToken: [],
   walletToken: 0,
-  nextToken: 0
+  nextToken: 0,
 };
 
 class App extends Component {
   web3Modal: Web3Modal;
   state: IAppState;
   walletMeta?: string;
-  
+
   constructor(props: any) {
     super(props);
     this.state = {
-      ...INITIAL_STATE
+      ...INITIAL_STATE,
     };
 
     this.web3Modal = new Web3Modal({
-      network: this.state.networkName || 'mainnet',
+      network: this.state.networkName || "mainnet",
       cacheProvider: true,
-      providerOptions: this.getProviderOptions()
+      providerOptions: this.getProviderOptions(),
     });
   }
 
   async componentDidMount() {
-    this.setState({nextToken: this._getNextRandom()})
-    await this.setup()
+    this.setState({ nextToken: this._getNextRandom() });
+    await this.setup();
   }
-  
+
   subscribeProvider = async (provider: any) => {
     if (!provider.on) {
       return;
     }
 
-    provider.on('close', () => {
+    provider.on("close", () => {
       this.resetApp(false);
     });
 
-    provider.on('disconnect', () => {
+    provider.on("disconnect", () => {
       this.resetApp(false);
     });
 
-    provider.on('accountsChanged', async (accounts: string[]) => {
+    provider.on("accountsChanged", async (accounts: string[]) => {
       if (accounts[0] !== this.state.address) {
-        this.setState({address : ethers.utils.getAddress(accounts[0])});
+        this.setState({ address: ethers.utils.getAddress(accounts[0]) });
       }
     });
 
-    provider.on('chainChanged', async (chainId: number) => {
+    provider.on("chainChanged", async (chainId: number) => {
       this.resetApp(false);
       await this.setup();
     });
 
-    provider.on('networkChanged', async () => {
+    provider.on("networkChanged", async () => {
       if (this.state.provider) {
         const network = await this.state.provider.getNetwork();
         if (network.chainId !== this.state.chainId) {
-          this.setState({chainId: network.chainId, networkName: network.name})
+          this.setState({
+            chainId: network.chainId,
+            networkName: network.name,
+          });
         }
       }
     });
   };
-  
+
   getProviderOptions = () => {
     const providerOptions = {
       walletconnect: {
         package: WalletConnectProvider,
         options: {
-          infuraId: process.env.REACT_APP_INFURA_ID
-        }
-      }
-    }
-    return providerOptions
-  }
-  
-  setup = async () => {
+          infuraId: process.env.REACT_APP_INFURA_ID,
+        },
+      },
+    };
+    return providerOptions;
+  };
 
+  setup = async () => {
     const web3Provider = await this.web3Modal.connect();
     await this.subscribeProvider(web3Provider);
     const provider = new ethers.providers.Web3Provider(web3Provider);
@@ -118,11 +119,11 @@ class App extends Component {
     const network = await provider.getNetwork();
     const chainId = network.chainId;
     const networkName = network.name;
-    
+
     const contract = new ethers.Contract(
       "0xd6a4e574a3cfd9d50143085d84b9176fe4b80c94",
       lootAbi,
-      provider
+      provider.getSigner(0)
     );
 
     await this.setState({
@@ -131,9 +132,10 @@ class App extends Component {
       address,
       chainId,
       networkName,
-      contract
+      contract,
     });
-  }
+    this._fillMintedToken(address, contract);
+  };
 
   resetApp = async (clearCache: boolean) => {
     this.state.provider?.removeAllListeners();
@@ -145,17 +147,17 @@ class App extends Component {
 
   _shortAddress(): string {
     const { address, networkName } = this.state;
-    return address !== ''
+    return address !== ""
       ? address.substring(0, 6) +
-          '...' +
+          "..." +
           address.substring(address.length - 4, address.length) +
-          ' (' +
+          " (" +
           networkName +
-          ')'
-      : 'CONNECT WALLET';
+          ")"
+      : "CONNECT WALLET";
   }
 
-  _fetcher = async (tokenId: number, cb: (meta:string)=>void) => {
+  _fetcher = async (tokenId: number, cb: (meta: string) => void) => {
     if (tokenId === 20000 && this.walletMeta) {
       cb(this.walletMeta);
     } else if (this.state.contract) {
@@ -165,10 +167,13 @@ class App extends Component {
       }
       cb(meta);
     }
-  }
-  
-  _getNextRandom = () : number => {
-    const minted = window.localStorage.getItem('mintedCustom')?.split(',').map((i) => parseInt(i));
+  };
+
+  _getNextRandom = (): number => {
+    const minted = window.localStorage
+      .getItem("mintedCustom")
+      ?.split(",")
+      .map((i) => parseInt(i));
     const all: number[] = [];
     let mintedPos = 0;
     for (let i = 0; i < 7778; ++i) {
@@ -179,33 +184,45 @@ class App extends Component {
       }
     }
     return 10000 + all[Math.floor(Math.random() * all.length)];
-  }
+  };
 
-  async _fillMintedToken() {
-    const { contract, address } = this.state;
+  async _fillMintedToken(address: string, contract: ethers.Contract) {
     const mt = [];
     let walletToken = 0;
 
     if (contract) {
       try {
-        for(let i = 0; i < 10; ++i) {
+        const num = (await contract.balanceOf(address)).toNumber();
+        for (let i = 0; i < num; ++i) {
           mt.push((await contract.tokenOfOwnerByIndex(address, i)).toNumber());
-          if (mt[mt.length - 1] >= 20000)
-            walletToken = mt[mt.length - 1];
+          if (mt[mt.length - 1] >= 20000) walletToken = mt[mt.length - 1];
         }
-      } catch(e) {}
+      } catch (e) {
+        console.log(e);
+      }
     }
-    this.setState({mintedToken: mt, walletToken});
+    this.setState({ mintedToken: mt, walletToken });
+  }
+
+  async _mint(tokenId: number) {
+    const { contract, address } = this.state;
+    if (contract) {
+      if (tokenId === 20000) {
+        await contract.walletClaim(address);
+      } else {
+        await contract.customClaim(tokenId);
+      }
+    }
   }
 
   render() {
-    const {connected, mintedToken, walletToken, nextToken } = this.state; 
+    const { connected, mintedToken, walletToken, nextToken } = this.state;
     return (
       <div className="App">
         <div className="App-nav">
           <button
-            className = "App-connect LOOT_BTN"
-            onClick = {connected ? () => this.resetApp(true) : () => this.setup()}
+            className="App-connect LOOT_BTN"
+            onClick={connected ? () => this.resetApp(true) : () => this.setup()}
           >
             {this._shortAddress()}
           </button>
@@ -214,14 +231,18 @@ class App extends Component {
           <LootBox
             tokenId={connected ? 20000 : 0}
             fetcher={this._fetcher}
-            disableText={walletToken ? 'TokenId: ' + walletToken : undefined}
+            disableText={walletToken ? "TokenId: " + walletToken : undefined}
+            mint={this._mint.bind(this)}
           />
           <img src={logo} className="App-logo" alt="logo" />
           <LootBox
             tokenId={connected ? nextToken : 0}
             fetcher={this._fetcher}
-            disableText = {mintedToken.length >= 5 ? 'You minted Max' : undefined}
-            newToken = {()=>{this.setState({nextToken: this._getNextRandom()})}}
+            disableText={mintedToken.length >= 5 ? "You minted Max" : undefined}
+            newToken={() => {
+              this.setState({ nextToken: this._getNextRandom() });
+            }}
+            mint={this._mint.bind(this)}
           />
         </header>
       </div>
--- src/components/LootBox.tsx
+++ src/components/LootBox.tsx
@@ -1,70 +1,77 @@
-import './LootBox.css';
+import "./LootBox.css";
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 type PROPS = {
   tokenId: number;
-  fetcher: (tokenId: number, cb: (meta: string)=>void ) => void;
+  fetcher: (tokenId: number, cb: (meta: string) => void) => void;
   newToken?: () => void;
+  mint: (tokenId: number) => void;
   disableText?: string;
 };
 
 type OSMTT = {
   trait_type: string;
   value: number;
-}
+};
 
 type OSM = {
   image: string;
   attributes: OSMTT[];
-}
+};
 
 function LootBox({
   tokenId,
   fetcher,
   newToken,
+  mint,
   disableText,
 }: PROPS): JSX.Element {
   const [metaJson, setMetaJson] = useState({} as OSM);
 
   useEffect(() => {
-    const setMetaData = (meta: string):void => {
-      const clearText = Buffer.from(meta, 'base64').toString('ascii');
+    const setMetaData = (meta: string): void => {
+      const clearText = Buffer.from(meta, "base64").toString("ascii");
       setMetaJson(JSON.parse(clearText));
-    }
+    };
     fetcher(tokenId, setMetaData);
     //Cleanup
-    return () => {}
+    return () => {};
   }, [fetcher, tokenId]);
- 
+
   return (
-    <div className = "lb_container">
-      {metaJson.image
-        ? (
+    <div className="lb_container">
+      {metaJson.image ? (
         <>
-          <div>{tokenId >= 20000 ? 'WALLET MINT' : 'FREE MINT'}</div>
-          <img alt={tokenId.toString()} src={metaJson.image}/>
-          <div className='lb_control'>
-            <div>{ metaJson.attributes[7].trait_type} : {metaJson.attributes[7].value}</div>
-            {disableText
-            ? disableText
-            : <button className="lb_button LOOT_BTN">Mint</button>
-            }
-            {tokenId < 20000 && (
-              <button 
+          <div>{tokenId >= 20000 ? "WALLET MINT" : "FREE MINT"}</div>
+          <img alt={tokenId.toString()} src={metaJson.image} />
+          <div className="lb_control">
+            <div>
+              {metaJson.attributes[7].trait_type} :{" "}
+              {metaJson.attributes[7].value}
+            </div>
+            {disableText ? (
+              disableText
+            ) : (
+              <button
                 className="lb_button LOOT_BTN"
-                onClick={newToken} 
+                onClick={() => mint(tokenId)}
               >
+                Mint
+              </button>
+            )}
+            {tokenId < 20000 && (
+              <button className="lb_button LOOT_BTN" onClick={newToken}>
                 {tokenId} (reload)
               </button>
             )}
           </div>
         </>
-        ) 
-        : 'Loading ...'
-      }
+      ) : (
+        "Loading ..."
+      )}
     </div>
-  )
+  );
 }
 
-export {LootBox};
+export { LootBox };
